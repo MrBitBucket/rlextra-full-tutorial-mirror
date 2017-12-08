e82db09dadd43f3757b1db2562fec8a7b689f141
robin <robin@reportlab.com>
2017-12-08 09:22:27 +0000

    changes to make the tutorial work in bot python 2.7 & >=3.3

--- product_catalog.py
+++ product_catalog.py
@@ -1,15 +1,12 @@
-import os
-import StringIO
-import urllib2
-import base64
-import pyRXPU
-import preppy
-import urllib2
-
+from __future__ import print_function
+import os, base64, preppy
 from xml.sax.saxutils import escape, unescape
 from rlextra.radxml.xmlutils import TagWrapper
 from rlextra.radxml.html_cleaner import cleanInline
 from rlextra.rml2pdf import rml2pdf
+import pyRXPU
+#required for python 2.7 & >=3.3
+from reportlab.lib.utils import getBytesIO, isUnicode, asUnicode, asNative, asBytes, isPy3, unicodeT
 
 DATA_DIR = 'data'
 
@@ -19,7 +16,7 @@ class Product(object):
 
 def fix(tag):
     "Apply fixes to their descriptive markup"
-    src = unicode(tag)
+    src = unicodeT(tag) #required for python 2.7 & >=3.3
     step1 = src.replace(u'\x82',u'&eacute;')
     step2 = unescape(step1)
     step3 = step2.encode('utf-8')
@@ -32,8 +29,8 @@ def parse_catalog(filename):
     We fully parse the XML and turn into Python variables, so that any encoding
     issues are confronted here rather than in the template
     """
-
     xml = open(filename).read()
+    if isUnicode(xml): xml = xml.encode('utf8') #required for python 2.7 & >=3.3
     p = pyRXPU.Parser()
     tree = p.parse(xml)
     tagTree = TagWrapper(tree)
@@ -58,7 +55,8 @@ def parse_catalog(filename):
 
         #originally the images came from a remote site.  We have stashed them in
         #the img/ subdirectory, so just chop off the final part of the path.
-        prod.image = os.path.split(fix(prodTag.ImageUrl))[-1].replace(' ','')
+        #asNative required for python 2.7 & >=3.3
+        prod.image = os.path.split(asNative(fix(prodTag.ImageUrl)))[-1].replace(' ','')
         
         if prod.modelNumber in request_a_quote:
             prod.price = "Call us on 01635 246830 for a quote"
@@ -88,23 +86,22 @@ def create_pdf(catalog, template):
         }
     rml = template.getOutput(namespace)
     open(os.path.join(DATA_DIR,'latest.rml'), 'w').write(rml)
-    buf = StringIO.StringIO()
-    rml2pdf.go(rml, outputFileName=buf)
+    buf = getBytesIO()
+    rml2pdf.go(asBytes(rml), outputFileName=buf)
     return buf.getvalue()
 
 def main():
     filename = os.path.join(DATA_DIR, 'products.xml')
-    print '\n'+'#'*20
-    print '\nabout to parse file: ', filename
+    print('\n'+'#'*20)
+    print('\nabout to parse file: ', filename)
     products = parse_catalog(filename)
-    print 'file parsed OK \n'
+    print('file parsed OK \n')
 
     
     pdf = create_pdf(products, 'flyer_template.prep')
     filename ='output/harwood_flyer.pdf'
     open(filename,'wb').write(pdf)
-    print 'Created %s' % filename
-
+    print('Created %s' % filename)
 
 if __name__ == '__main__':
     main()
